1b2e9818814c7646c8551c85a6480d384c663006
Kacper Kwiatkowski <kacper18k@gmail.com>
2020-12-17 17:34:04 +0100

    Minor tweeks

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
                         target="_blank"
                         rel="noopener noreferrer"
                     >
-                        Changes so the docker container would be rebuild
+                        Fib calculator
                     </a>
                     <Link to="/">Home</Link>
                     <Link to="/otherpage">Other Page</Link>
