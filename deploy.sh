19a61ec78c51a535ded847f628887e13a67cf9b5
Kacper Kwiatkowski <kacper18k@gmail.com>
2020-12-29 17:23:44 +0100

    Deploy file set up.

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t kacperkwiatkowski/fibonacci_docker-client:latest -t kacperkwiatkowski/fibonacci_docker:$SHA -f ./client/Dockerfile ./client
+docker build -t kacperkwiatkowski/fibonacci_docker-server:latest -t kacperkwiatkowski/fibonacci_docker:$SHA -f ./server/Dockerfile ./server
+docker build -t kacperkwiatkowski/fibonacci_docker-worker:latest -t kacperkwiatkowski/fibonacci_docker:$SHA -f ./client/Dockerfile ./worker
+
+docker push kacperkwiatkowski/fibonacci_docker-client:latest
+docker push kacperkwiatkowski/fibonacci_docker-server:latest
+docker push kacperkwiatkowski/fibonacci_docker-worker:latest
+
+docker push kacperkwiatkowski/fibonacci_docker-client:$SHA
+docker push kacperkwiatkowski/fibonacci_docker-server:$SHA
+docker push kacperkwiatkowski/fibonacci_docker-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=kacperkwiatkowski/fibonacci_docker-server:$SHA
+kubectl set image deployments/client-deployment client=kacperkwiatkowski/fibonacci_docker-client:$SHA
+kubectl set image deployments/worker-deployment worker=kacperkwiatkowski/fibonacci_docker-worker:$SHA
